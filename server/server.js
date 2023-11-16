35eccb84938b6c310ee1c3a062b145540463b016
khanummer <ummernkhan@gmail.com>
2023-11-16 01:02:24 -0800

    adds backend API and frontend connectivity. Showing the array of userss on the backend on the frontend through state

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json{"users": ["userOne", "userTwo", "userThree"]}
+    res.json({"users": ["userOne", "userTwo", "userThree"]})
 });
 
-app.listen(5000, () => {console.log("Server started on port 5000")});
+app.listen(4000, () => {console.log("Server started on port 4000")});
